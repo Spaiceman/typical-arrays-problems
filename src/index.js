2d72c2b81030904e95ebee5ad86bc64cf0a4871c
Spaiceman <76124405+Spaiceman@users.noreply.github.com>
2021-03-12 00:37:31 +0300

    init: Start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array ==0 ||array==undefined) return 0;
+    let i= [""];
+    let minimum= array[0];
+
+       for (i=1;i<array.length; i++) {
+       if (array[i]<minimum) minimum = array[i];
+   }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array ==0 ||array==undefined) return 0;
+    let i= [""];
+    let maximum= array[0];
+
+       for (i=1;i<array.length; i++) {
+       if (array[i]>maximum) maximum = array[i];
+   }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array ==0 ||array==undefined) return 0;
+    let i= [""];
+    let elements=0;
+      for (i=0;i<array.length; i++) {
+        elements=  elements+array[i];
+   }
+  return (elements/array.length);
 }
